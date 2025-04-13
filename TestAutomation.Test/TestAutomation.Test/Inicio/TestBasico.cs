064271031d21768dc36207e19263a30c75c85388
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-04-12 20:24:13 -0500

    Ubicar por id el elemento de la pagina y hacerle click

--- TestAutomation.Test/TestAutomation.Test/Inicio/TestBasico.cs
+++ TestAutomation.Test/TestAutomation.Test/Inicio/TestBasico.cs
@@ -1,5 +1,7 @@
 ﻿using OpenQA.Selenium.Chrome;
 using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Xml.Linq;
 
 namespace TestAutomation.Test.Inicio
 {
@@ -12,6 +14,8 @@ namespace TestAutomation.Test.Inicio
             var driver = new ChromeDriver();
             driver.Manage().Window.Maximize(); // sentencia para maximizar navegador
             driver.Url = "https://curso.testautomation.es"; //para navegar a la pj web que vamos a testear
+            var normalLoadWeb = driver.FindElement(By.Id("NormalWeb"));// para ubicar por id el elemento de la pagina
+            normalLoadWeb.Click();// para hacer click en el elemento.
         }
     }
 }
