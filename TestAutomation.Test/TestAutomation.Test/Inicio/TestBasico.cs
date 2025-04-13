ccb5f651341619985b92951c88d95e40d25799c7
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-04-12 20:31:04 -0500

    Obtener el tag h1, obtener su vaalor y liberar recursos

--- TestAutomation.Test/TestAutomation.Test/Inicio/TestBasico.cs
+++ TestAutomation.Test/TestAutomation.Test/Inicio/TestBasico.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using System.Xml.Linq;
+using FluentAssertions;
 
 namespace TestAutomation.Test.Inicio
 {
@@ -16,6 +17,9 @@ namespace TestAutomation.Test.Inicio
             driver.Url = "https://curso.testautomation.es"; //para navegar a la pj web que vamos a testear
             var normalLoadWeb = driver.FindElement(By.Id("NormalWeb"));// para ubicar por id el elemento de la pagina
             normalLoadWeb.Click();// para hacer click en el elemento.
+            var titulo = driver.FindElement(By.CssSelector("h1"));//para obtener el elemento que tine el tag h1
+            titulo.Text.Should().Be("Normal load website"); //para obtener el valor texto.
+            driver.Quit(); //para liberar los recursos
         }
     }
 }
