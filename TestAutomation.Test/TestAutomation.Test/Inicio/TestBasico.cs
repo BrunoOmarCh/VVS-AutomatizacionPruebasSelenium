c77aed6cf9af903abd7d2a65f99b0c6642dfbf79
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-04-13 13:21:16 -0500

    Obtiene el title, el valor del texto y libera recurso

--- TestAutomation.Test/TestAutomation.Test/Inicio/TestBasico.cs
+++ TestAutomation.Test/TestAutomation.Test/Inicio/TestBasico.cs
@@ -31,6 +31,7 @@ namespace TestAutomation.Test.Inicio
             driver.Url = "https://curso.testautomation.es"; //para navegar a la pj web que vamos a testear
             var slowLoadWeb = driver.FindElement(By.Id("SlowLoadWeb"));// para ubicar por id el elemento de la pagina
             slowLoadWeb.Click();// para hacer click en el elemento.
+            Thread.Sleep(3000);// esto pausa la ejecucion x 3 segundos
             var titulo = driver.FindElement(By.Id("title"));//para obtener el elemento que tiene title
             titulo.Text.Should().Be("Slow load website"); //para obtener el valor texto.
             driver.Quit(); //para liberar los recursos
