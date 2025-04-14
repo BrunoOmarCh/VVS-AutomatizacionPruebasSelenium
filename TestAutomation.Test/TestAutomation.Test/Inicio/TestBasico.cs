5f48672130f3883f498dfbc3b43db8ffe4e9f66f
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-04-14 13:48:29 -0500

    Comentarios de funcionamiento y anotaciones

--- TestAutomation.Test/TestAutomation.Test/Inicio/TestBasico.cs
+++ TestAutomation.Test/TestAutomation.Test/Inicio/TestBasico.cs
@@ -32,6 +32,8 @@ namespace TestAutomation.Test.Inicio
             var slowLoadWeb = driver.FindElement(By.Id("SlowLoadWeb"));
             slowLoadWeb.Click();
             Thread.Sleep(3000);// esto pausa la ejecucion x 3 segundos
+            //Falla porque la página tarda unos segundos en cargar, Selenium intenta ubicar el elemento inmediatamente
+            //Lo que provoca un error porque aún no ha aparecido en el DOM.
             var titulo = driver.FindElement(By.Id("title"));//para obtener el elemento que tiene title
             titulo.Text.Should().Be("Slow load website"); //para obtener el valor texto.
             driver.Quit(); 
@@ -45,6 +47,9 @@ namespace TestAutomation.Test.Inicio
             driver.Url = "https://curso.testautomation.es"; //para navegar a la pj web que vamos a testear
             var slowLoadTextWeb = driver.FindElement(By.Id("SlowSpeedTextWeb")); // para ubicar por id el elemento de la pagina
             slowLoadTextWeb.Click();
+            Thread.Sleep(1500);
+            //La página carga el texto del título con un pequeño retardo (1 segundo) .
+            //El test falla porque intenta leer el texto antes de que se haya cargado.
             var titulo = driver.FindElement(By.Id("title"));
             titulo.Text.Should().Be("Slow load website");
             driver.Quit();
