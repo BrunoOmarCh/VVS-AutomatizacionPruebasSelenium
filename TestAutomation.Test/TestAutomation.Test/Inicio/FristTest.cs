5fa379df7afe8afc5cca128de466a0513b15dd96
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-04-14 14:45:35 -0500

    Primera parte del test

---
+++ TestAutomation.Test/TestAutomation.Test/Inicio/FristTest.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Xml.Linq;
+using FluentAssertions;
+
+namespace TestAutomation.Test.Inicio
+{
+    public class FristTest
+    {          
+
+    }
+}
