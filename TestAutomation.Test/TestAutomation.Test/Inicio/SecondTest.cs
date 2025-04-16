b09f36f0f580ab8589ec81ce470bdcdea96c7ffb
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-04-16 16:03:22 -0500

    Configura instancia de ChromeDriver y maximiza la ventana

--- TestAutomation.Test/TestAutomation.Test/Inicio/SecondTest.cs
+++ TestAutomation.Test/TestAutomation.Test/Inicio/SecondTest.cs
@@ -11,6 +11,16 @@ namespace TestAutomation.Test.Inicio
 {
     public class SecondTest
     {
-
+        #pragma warning disable NUnit1032
+        ChromeDriver driver;
+        [SetUp]
+        public void SetUp()
+        {
+            // todo lo que este dentro del SetUp se ejecutara antes de cualquier metodo
+            //1. Resolver la duplicidad de codigo
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize(); // sentencia para maximizar navegador
+            driver.Url = "https://curso.testautomation.es"; //para navegar a la pj web que vamos a testear
+        }
     }
 }
