3b462375bfa5733d112d8607dd52c62d1926d554
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-04-16 16:04:48 -0500

    Agrega test para validar carga lenta del sitio con espera manual

--- TestAutomation.Test/TestAutomation.Test/Inicio/SecondTest.cs
+++ TestAutomation.Test/TestAutomation.Test/Inicio/SecondTest.cs
@@ -38,6 +38,18 @@ namespace TestAutomation.Test.Inicio
             var titulo = driver.FindElement(By.CssSelector("h1"));//para obtener el elemento que tine el tag h1
             titulo.Text.Should().Be("Normal load website"); //para obtener el valor texto.
         }
+        [Test]
+        public void TestSlowLoadWebPage()
+        {
+            //Slow load website
+            var slowLoadWeb = driver.FindElement(By.Id("SlowLoadWeb"));
+            slowLoadWeb.Click();
+            Thread.Sleep(3000);// esto pausa la ejecucion x 3 segundos
+            //Falla porque la página tarda unos segundos en cargar, Selenium intenta ubicar el elemento inmediatamente
+            //Lo que provoca un error porque aún no ha aparecido en el DOM.
+            var titulo = driver.FindElement(By.Id("title"));//para obtener el elemento que tiene title
+            titulo.Text.Should().Be("Slow load website"); //para obtener el valor texto.
+        }
 
     }
 }
