1ea753511f1a8b53b0756a48cd7689760f6f6d55
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-04-17 16:50:00 -0500

    Configura ChromeDriver en método SetUp con maximización y espera implícita

--- TestAutomation.Test/TestAutomation.Test/Inicio/ThreeCode.cs
+++ TestAutomation.Test/TestAutomation.Test/Inicio/ThreeCode.cs
@@ -10,8 +10,21 @@ using FluentAssertions;
 
 namespace TestAutomation.Test.Inicio
 {
+    [TestFixture]
     public class ThreeCode
     {
-        [TestFixture]
+        #pragma warning disable NUnit1032
+        ChromeDriver driver;
+        [SetUp]
+        public void SetUp()
+        {
+            // todo lo que este dentro del SetUp se ejecutara antes de cualquier metodo
+            //1. Resolver la duplicidad de codigo
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize(); // sentencia para maximizar navegador
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+            //Esta indica a Selenium q cuando busqye un elemento lo haga por 3 segundos
+            driver.Url = "https://curso.testautomation.es"; //para navegar a la pj web que vamos a testear
+        }
     }
 }
