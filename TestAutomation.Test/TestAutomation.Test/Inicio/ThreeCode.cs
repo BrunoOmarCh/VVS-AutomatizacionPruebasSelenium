71b96f9ded4bc6826726a720162335ca4e3cf295
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-04-17 16:47:09 -0500

    Agrega imports necesarios para pruebas automatizadas con Selenium y NUnit

--- TestAutomation.Test/TestAutomation.Test/Inicio/ThreeCode.cs
+++ TestAutomation.Test/TestAutomation.Test/Inicio/ThreeCode.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 
 namespace TestAutomation.Test.Inicio
 {
