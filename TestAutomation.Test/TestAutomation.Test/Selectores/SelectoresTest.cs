b159f0da4b46957e48b05dcceddee54f481465b8
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-04-18 17:18:58 -0500

    Configura ChromeDriver en método SetUp con maximización y espera implícita

--- TestAutomation.Test/TestAutomation.Test/Selectores/SelectoresTest.cs
+++ TestAutomation.Test/TestAutomation.Test/Selectores/SelectoresTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace TestAutomation.Test.Selectores
 {
-    internal class SelectoresTest
+    public class SelectoresTest
     {
+        #pragma warning disable NUnit1032
+        IWebDriver driver;
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+            driver.Url = "https://curso.testautomation.es";
+        }
     }
+
 }
+
