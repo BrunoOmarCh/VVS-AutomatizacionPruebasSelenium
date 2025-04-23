1e42070a634eb88e585fdc851a4c3bf9fe6358ee
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-04-22 19:45:58 -0500

    Cambio de restriccion, declaracion de variable global

--- TestAutomation.Test/TestAutomation.Test/Frame/FrameTests.cs
+++ TestAutomation.Test/TestAutomation.Test/Frame/FrameTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace TestAutomation.Test.Frame
 {
-    internal class FrameTests
-    {   
+    public class FrameTests
+    {
+        #pragma warning disable NUnit1032
+        IWebDriver driver;
     }
 }
