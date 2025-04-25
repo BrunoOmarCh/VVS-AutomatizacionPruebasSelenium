cf472b620538b0ed4f8c28cb928e8487a79e6c3b
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-04-25 15:21:09 -0500

    Definir IwebElement como fruitWebElement

--- TestAutomation.Test/TestAutomation.Test/PageObjetPattern/PageObject/HomePage/FruitWebElement.cs
+++ TestAutomation.Test/TestAutomation.Test/PageObjetPattern/PageObject/HomePage/FruitWebElement.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,10 @@ namespace TestAutomation.Test.PageObjetPattern.PageObject.HomePage
 {
     public class FruitWebElement
     {
+        //De cada fruta necesitamos : Nombre, Precio y descripción, para luego crear un nuevo Fruit model
+        //trabajar sobre un IwebElement
+        private readonly IWebElement fruitWebElement;
+
+
     }
 }
