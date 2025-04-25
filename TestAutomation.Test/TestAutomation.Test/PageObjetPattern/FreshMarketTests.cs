853a9d444f4eff2a609aaf0f879d7b38d1e894cd
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-04-24 19:05:49 -0500

    Add method Verify Fruits are Correctly Dislayed

--- TestAutomation.Test/TestAutomation.Test/PageObjetPattern/FreshMarketTests.cs
+++ TestAutomation.Test/TestAutomation.Test/PageObjetPattern/FreshMarketTests.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestAutomation.Test.PageObjetPattern.Models;
 
 namespace TestAutomation.Test.PageObjetPattern
 {
-    internal class FreshMarketTests
+    public class FreshMarketTests
     {
+        /// <summary>
+        /// Verify that the next provided fruits are displayed into the shop.
+        /// Please check that the content of all fruits are correct.
+        /// </summary>
+        [Test]
+        public void VerifyThatFruitsAreCorrectlyDisplayed()
+        {
+
+        }
     }
 }
