544b40e1e8d8d8279018099127297f266cbda1c3
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-04-26 18:45:45 -0500

    feat(helper): añadir método que convierte IList<IWebElement> a IList<FruitWebElement>

---
+++ TestAutomation.Test/TestAutomation.Test/PageObjetPattern/Helpers/FruitHelpers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using OpenQA.Selenium;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestAutomation.Test.PageObjetPattern.Models;
+using TestAutomation.Test.PageObjetPattern.PageObject.HomePage;
+
+namespace TestAutomation.Test.PageObjetPattern.Helpers
+{
+    public class FruitHelpers
+    {
+        // Convierte una lista de IWebElement en una lista de FruitWebElement.
+        public static IList<FruitWebElement> Parse(IList<IWebElement> fruits)
+        {
+            return fruits.Select(fruit => new FruitWebElement(fruit)).ToList();
+        }
+
+
+    }
+}
