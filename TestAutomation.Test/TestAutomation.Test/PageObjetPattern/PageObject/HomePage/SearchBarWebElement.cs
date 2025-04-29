a08504e90ea9b44b9d409794ec08cac33c3569f2
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-04-28 22:48:31 -0500

    Constructor de la clase

--- TestAutomation.Test/TestAutomation.Test/PageObjetPattern/PageObject/HomePage/SearchBarWebElement.cs
+++ TestAutomation.Test/TestAutomation.Test/PageObjetPattern/PageObject/HomePage/SearchBarWebElement.cs
@@ -6,6 +6,11 @@ namespace TestAutomation.Test.PageObjetPattern.PageObject.HomePage
     public class SearchBarWebElement
     {
         private readonly IWebDriver driver;
+        // el constrctor
+        public SearchBarWebElement(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
 
     }
 }
