c4c51b6a79f20190e37e0f3ae7ec8448d790dd74
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-05-02 20:40:55 -0500

    feat(model): agrega propiedad Description en FruitModel // Descripción o detalles de la fruta.

--- TestAutomation.Test/TestAutomation.Test/PageObjetPattern/Models/FruitModel.cs
+++ TestAutomation.Test/TestAutomation.Test/PageObjetPattern/Models/FruitModel.cs
@@ -10,6 +10,6 @@ namespace TestAutomation.Test.PageObjetPattern.Models
     {
         public string Name { get; set; }
         public decimal Price { get; set; }
-
+        public string Description { get; set; }
     }
 }
