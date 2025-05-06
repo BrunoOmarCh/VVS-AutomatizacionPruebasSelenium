a17d8edd52dfc78a740405f1719ece535eb330da
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-05-06 16:02:41 -0500

    feat(searchbar): SearchBarWebElement // clase para barra de búsqueda

--- TestAutomation.Test/TestAutomation.Test/PageObjetPattern/PageObject/HomePage/SearchBarWebElement.cs
+++ TestAutomation.Test/TestAutomation.Test/PageObjetPattern/PageObject/HomePage/SearchBarWebElement.cs
@@ -5,35 +5,6 @@ namespace TestAutomation.Test.PageObjetPattern.PageObject.HomePage
 {
     public class SearchBarWebElement
     {
-        private readonly IWebDriver driver;
-        // el constrctor
-        public SearchBarWebElement(IWebDriver driver)
-        {
-            this.driver = driver;
-        }
-        // la barra de input y boton sarch esta no sera publica
-        private IWebElement InputSearchProduct => driver.FindElement(By.Id("product-search"));
-        private IWebElement ButtonSearch => driver.FindElement(By.Id("search-button"));
-        // acciones del input y boton search
-        public SearchBarWebElement InputSearch(string termToSearch)
-        {
-            InputSearchProduct.Clear();
-            InputSearchProduct.SendKeys(termToSearch);
-            return this;
-        }
-        //pulsamos click sobre el boton
-        public HomePageObject ClickSearch()
-        {
-            ButtonSearch.Click();
-            return new HomePageObject(driver);
-        }
-        //pulsando Enter
-        public HomePageObject ClickEnter()
-        {
-            new Actions(driver)
-            .SendKeys(Keys.Enter)
-            .Perform();
-            return new HomePageObject(driver);
-        }
+
     }
 }
