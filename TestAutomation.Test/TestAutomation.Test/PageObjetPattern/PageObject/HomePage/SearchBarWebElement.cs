50a9b50da0b30752786cacd6377b8c7fe8689943
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-05-06 16:03:04 -0500

    feat(searchbar): SearchBarWebElement // clase para barra de búsqueda

--- TestAutomation.Test/TestAutomation.Test/PageObjetPattern/PageObject/HomePage/SearchBarWebElement.cs
+++ TestAutomation.Test/TestAutomation.Test/PageObjetPattern/PageObject/HomePage/SearchBarWebElement.cs
@@ -5,6 +5,12 @@ namespace TestAutomation.Test.PageObjetPattern.PageObject.HomePage
 {
     public class SearchBarWebElement
     {
+        private readonly IWebDriver driver;
+        // el constrctor
+        public SearchBarWebElement(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
 
     }
 }
