d0bc75599e307f996354e8d1a55bfa4797783fbc
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-05-09 15:24:23 -0500

    feat(wait): Stopwatch // mide tiempo transcurrido en espera activa

--- TestAutomation.Test/TestAutomation.Test/PageObjetPattern/Helpers/WaitHelper.cs
+++ TestAutomation.Test/TestAutomation.Test/PageObjetPattern/Helpers/WaitHelper.cs
@@ -10,6 +10,10 @@ namespace TestAutomation.Test.PageObjetPattern.Helpers
     public static class WaitHelper
     {
         public static void WaitForCondition(Func<bool> condition, int msTimeout = 4000)
+        {
+            // este codigo es muy util para controlar I
+            var stopWatch = new Stopwatch(); // definimos una variable de tipo Stopwatch
+            stopWatch.Start(); //iniciamos la variable.
 
     }
 }
