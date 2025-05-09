6dc705827bfeae4527fd06f0331d7acbb0adc870
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-05-09 15:23:42 -0500

    feat(wait): WaitForCondition() // ejecuta condición hasta timeout o éxito

--- TestAutomation.Test/TestAutomation.Test/PageObjetPattern/Helpers/WaitHelper.cs
+++ TestAutomation.Test/TestAutomation.Test/PageObjetPattern/Helpers/WaitHelper.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TestAutomation.Test.PageObjetPattern.Helpers
 {
-    public class WaitHelper
+    public static class WaitHelper
     {
+        public static void WaitForCondition(Func<bool> condition, int msTimeout = 4000)
+
     }
 }
