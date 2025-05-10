63d515cb3fb55de2c254f3d035efd8a56e962217
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-05-10 15:28:21 -0500

    feat(cart): constructor ShoppingCartPageObject(IWebDriver) // inicializa con driver

--- TestAutomation.Test/TestAutomation.Test/PageObjetPattern/PageObject/ShoppingCart/ShoppingCartPageObject.cs
+++ TestAutomation.Test/TestAutomation.Test/PageObjetPattern/PageObject/ShoppingCart/ShoppingCartPageObject.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace TestAutomation.Test.PageObjetPattern.PageObject.ShoppingCart
 {
     public class ShoppingCartPageObject
     {
+
+        public ShoppingCartPageObject(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
     }
 }
