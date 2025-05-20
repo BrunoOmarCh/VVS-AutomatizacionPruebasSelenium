64d825b568d72f8c50f53b411ff1185e1e05bd68
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-05-20 16:06:16 -0500

    feat(uicontext): UITestContext // clase gestora del WebDriver

---
+++ TestAutomation.Test/TestAutomation.Test/PageObjetPattern/Helpers/UITestContext.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestAutomation.Test.PageObjetPattern.Factories;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+
+namespace TestAutomation.Test.PageObjetPattern.Helpers
+{
+    public class UITestContext : IDisposable
+    {
+
+    }
+}
