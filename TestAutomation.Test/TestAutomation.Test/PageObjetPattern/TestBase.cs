f756f1bab66fbf846166c15d5a2fc4a4f9395cd6
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-05-22 16:12:15 -0500

    feat(testbase): clase base TestBase con configuración global para pruebas

--- TestAutomation.Test/TestAutomation.Test/PageObjetPattern/TestBase.cs
+++ TestAutomation.Test/TestAutomation.Test/PageObjetPattern/TestBase.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestAutomation.Test.PageObjetPattern.Models;
 
 namespace TestAutomation.Test.PageObjetPattern
 {
-    internal class TestBase
+    [Parallelizable(ParallelScope.All)]
+    public class TestBase
     {
+
     }
 }
