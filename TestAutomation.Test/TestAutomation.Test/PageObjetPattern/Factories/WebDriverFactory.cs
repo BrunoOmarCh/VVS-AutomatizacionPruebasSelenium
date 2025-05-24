38d731d3b2077665c698e18753fdf09c6180da3e
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-05-24 16:16:17 -0500

    feat(factory): clase WebDriverFactory estática para instanciar navegadores

--- TestAutomation.Test/TestAutomation.Test/PageObjetPattern/Factories/WebDriverFactory.cs
+++ TestAutomation.Test/TestAutomation.Test/PageObjetPattern/Factories/WebDriverFactory.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,8 @@ using System.Threading.Tasks;
 
 namespace TestAutomation.Test.PageObjetPattern.Factories
 {
-    internal class WebDriverFactory
+    public static class WebDriverFactory
     {
+
     }
 }
