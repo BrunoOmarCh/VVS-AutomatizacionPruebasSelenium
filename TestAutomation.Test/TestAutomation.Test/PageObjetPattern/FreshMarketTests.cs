662015026898c9fd2e41fb3e924ce0d3ca8fa9fd
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-05-25 16:22:02 -0500

    Definir el url

--- TestAutomation.Test/TestAutomation.Test/PageObjetPattern/FreshMarketTests.cs
+++ TestAutomation.Test/TestAutomation.Test/PageObjetPattern/FreshMarketTests.cs
@@ -51,7 +51,7 @@ namespace TestAutomation.Test.PageObjetPattern
             var driver = uiTestContext.Driver;
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
-
+            driver.Url = "https://curso.testautomation.es/FruitVegetablesShopWeb/index.html";
 
             var expectedFruits = new List<FruitModel>
             {
