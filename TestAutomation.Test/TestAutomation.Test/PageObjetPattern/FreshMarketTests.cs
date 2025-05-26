bc78d426463b2f266eba4d775f7e004d59c090d5
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-05-26 16:27:22 -0500

    test(search): inicia SearchTests con UITestContext y HomePageObject

--- TestAutomation.Test/TestAutomation.Test/PageObjetPattern/FreshMarketTests.cs
+++ TestAutomation.Test/TestAutomation.Test/PageObjetPattern/FreshMarketTests.cs
@@ -109,6 +109,7 @@ namespace TestAutomation.Test.PageObjetPattern
         {
             using UITestContext uiTestContext = new UITestContext();
             var driver = uiTestContext.Driver;
+            var homePage = new HomePageObject(driver); // no retorna la pagina
 
         }
     }
