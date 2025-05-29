577f66e7ef8d601fe7b6de85ba0ad04868fdfe13
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-05-29 16:39:36 -0500

    test(cart): navega a página 3

--- TestAutomation.Test/TestAutomation.Test/PageObjetPattern/FreshMarketTests.cs
+++ TestAutomation.Test/TestAutomation.Test/PageObjetPattern/FreshMarketTests.cs
@@ -162,6 +162,7 @@ namespace TestAutomation.Test.PageObjetPattern
             expectedFruitsInCart.Add(AddItemToCart(DisplayedFruits(), "Banana", 6));
             homePage.PageNavegation.ClickButtonPage2(); //estamos en pagina 2
             expectedFruitsInCart.Add(AddItemToCart(DisplayedFruits(), "Avocado", 5));
+            homePage.PageNavegation.ClickButtonPage3(); //estamos en pagina 3
 
 
 
