b44141493193adc84f9721e0e092290035e85d3e
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-06-06 17:11:11 -0500

    chore(helper): importa librerías necesarias para WebDriver y configuración base

--- TestAutomation.Test/TestAutomation.Test/VRP/Helpers/UITestContextVRP.cs
+++ TestAutomation.Test/TestAutomation.Test/VRP/Helpers/UITestContextVRP.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestAutomation.Test.PageObjetPattern.Factories;
+using TestAutomation.Test.PageObjetPattern;
 
 namespace TestAutomation.Test.VRP.Helpers
 {
