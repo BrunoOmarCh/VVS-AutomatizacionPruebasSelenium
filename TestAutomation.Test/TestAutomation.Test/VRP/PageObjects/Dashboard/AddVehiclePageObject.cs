93bc75557c17510a001290962b485eded6dc8f7f
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-06-09 17:24:22 -0500

    feat(pageobject): agrega métodos públicos para seleccionar la primera opción en dropdowns de Modelo

--- TestAutomation.Test/TestAutomation.Test/VRP/PageObjects/Dashboard/AddVehiclePageObject.cs
+++ TestAutomation.Test/TestAutomation.Test/VRP/PageObjects/Dashboard/AddVehiclePageObject.cs
@@ -49,5 +49,6 @@ namespace TestAutomation.Test.VRP.PageObjects.Dashboard
 
         // ------------ métodos públicos fluidos -------------
         public AddVehiclePageObject SelectFirstMarca() => SelectFirst("marca");
+        public AddVehiclePageObject SelectFirstModelo() => SelectFirst("modelo");
     }
 }
