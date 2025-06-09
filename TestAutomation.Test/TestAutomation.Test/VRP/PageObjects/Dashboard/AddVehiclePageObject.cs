ce3b6bf2520f6e3d3f09260d9828453a9ed258e1
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-06-09 17:19:10 -0500

    chore: importa dependencias necesarias para WebDriver, helpers y modelos

--- TestAutomation.Test/TestAutomation.Test/VRP/PageObjects/Dashboard/AddVehiclePageObject.cs
+++ TestAutomation.Test/TestAutomation.Test/VRP/PageObjects/Dashboard/AddVehiclePageObject.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestAutomation.Test.PageObjetPattern.Helpers;
+using TestAutomation.Test.PageObjetPattern.Models;
 
 namespace TestAutomation.Test.VRP.PageObjects.Dashboard
 {
-    internal class AddVehiclePageObject
+    /// <summary>
+    /// Page Object para el formulario "Agregar Vehículo".
+    /// </summary>
+    public class AddVehiclePageObject
     {
+
     }
 }
