792494170bb990256db2143e7025f1f59fbc8341
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-06-09 17:21:16 -0500

    feat(pageobject): espera hasta que las opciones estén visibles y selecciona la primera opción del dropdown

--- TestAutomation.Test/TestAutomation.Test/VRP/PageObjects/Dashboard/AddVehiclePageObject.cs
+++ TestAutomation.Test/TestAutomation.Test/VRP/PageObjects/Dashboard/AddVehiclePageObject.cs
@@ -25,5 +25,27 @@ namespace TestAutomation.Test.VRP.PageObjects.Dashboard
             var btn = driver.FindElement(By.CssSelector($"label[for='{labelFor}'] + .custom-select .select-button"));
             btn.Click();
 
+            // 2. esperar elementos visibles solo dentro de ESE dropdown
+            WaitHelper.WaitForCondition(() =>
+            {
+                var container = btn.FindElement(By.XPath("./following-sibling::div[contains(@class,'select-content')]"));
+
+                var items = container.FindElements(By.CssSelector(".select-item")).Where(e => e.Displayed).ToList();
+                if (!items.Any()) return false;
+
+                try
+                {
+                    items.First().Click();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }, timeoutMs);
+
+            return this;
+        }
+
     }
 }
