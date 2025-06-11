efc83b4b60234ea4c6b6fac06620e3e60ecfc214
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-06-11 17:27:50 -0500

    chore: importa WebDriver, helpers y modelos necesarios para PageObject Dashboard

--- TestAutomation.Test/TestAutomation.Test/VRP/PageObjects/Dashboard/DashboardPageObject.cs
+++ TestAutomation.Test/TestAutomation.Test/VRP/PageObjects/Dashboard/DashboardPageObject.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestAutomation.Test.PageObjetPattern.Helpers;
+using TestAutomation.Test.PageObjetPattern.Models;
 
 namespace TestAutomation.Test.VRP.PageObjects.Dashboard
 {
