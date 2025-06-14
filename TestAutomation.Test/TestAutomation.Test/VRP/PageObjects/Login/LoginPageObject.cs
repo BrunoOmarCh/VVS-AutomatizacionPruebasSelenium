2ff6bdadff8cdbfca04596171a29f3397bc46ef6
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-06-14 17:41:03 -0500

    feat: encapsula selectores del formulario de login (email, password, botón)

--- TestAutomation.Test/TestAutomation.Test/VRP/PageObjects/Login/LoginPageObject.cs
+++ TestAutomation.Test/TestAutomation.Test/VRP/PageObjects/Login/LoginPageObject.cs
@@ -10,5 +10,12 @@ namespace TestAutomation.Test.VRP.PageObjects.Login
 {
     public class LoginPageObject
     {
+        private readonly IWebDriver driver;
+        private readonly int timeoutMs = 10000;
+
+        private By EmailInput => By.CssSelector(".formBox_input input[type='email']");
+        private By PasswordInput => By.CssSelector(".formBox_input input[type='password']");
+        private By LoginButton => By.CssSelector(".formBox_button");
+
     }
 }
