6bf1794c714ddec41a6c02d70278e70c743e5176
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-06-14 17:39:42 -0500

    chore: crea archivo LoginPageObject y agrega dependencias necesarias

--- TestAutomation.Test/TestAutomation.Test/VRP/PageObjects/Login/LoginPageObject.cs
+++ TestAutomation.Test/TestAutomation.Test/VRP/PageObjects/Login/LoginPageObject.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestAutomation.Test.PageObjetPattern.Helpers;
 
 namespace TestAutomation.Test.VRP.PageObjects.Login
 {
