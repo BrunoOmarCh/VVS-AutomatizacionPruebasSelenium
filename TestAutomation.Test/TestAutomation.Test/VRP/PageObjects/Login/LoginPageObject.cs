14f48a1e71b0f0ad0348572d3c296517fac7c40d
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-06-14 17:41:34 -0500

    feat: implementa constructor que recibe instancia de WebDriver

--- TestAutomation.Test/TestAutomation.Test/VRP/PageObjects/Login/LoginPageObject.cs
+++ TestAutomation.Test/TestAutomation.Test/VRP/PageObjects/Login/LoginPageObject.cs
@@ -10,12 +10,19 @@ namespace TestAutomation.Test.VRP.PageObjects.Login
 {
     public class LoginPageObject
     {
+        /// Formulario de login de RoadMap Pro.
+
         private readonly IWebDriver driver;
         private readonly int timeoutMs = 10000;
 
         private By EmailInput => By.CssSelector(".formBox_input input[type='email']");
         private By PasswordInput => By.CssSelector(".formBox_input input[type='password']");
         private By LoginButton => By.CssSelector(".formBox_button");
+
+        public LoginPageObject(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
 
     }
 }
