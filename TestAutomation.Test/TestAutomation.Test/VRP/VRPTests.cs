dbe68a12012120cc1134d06c0a7e9926caa1fd7b
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2025-06-15 17:45:34 -0500

    chore(vrp): agrega using de Selenium, helpers y PageObjects requeridos

--- TestAutomation.Test/TestAutomation.Test/VRP/VRPTests.cs
+++ TestAutomation.Test/TestAutomation.Test/VRP/VRPTests.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestAutomation.Test.PageObjetPattern;
+using TestAutomation.Test.PageObjetPattern.Helpers;
+using TestAutomation.Test.VRP.Helpers;
+using TestAutomation.Test.VRP.PageObjects.Dashboard;
+using TestAutomation.Test.VRP.PageObjects.Home;
 
 namespace TestAutomation.Test.VRP
 {
